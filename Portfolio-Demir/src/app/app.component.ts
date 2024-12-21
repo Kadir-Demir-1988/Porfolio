2ec2ce120383224c9dbfd87b6f2921f61597da0b
Kadir-Demir-1988 <163757670+Kadir-Demir-1988@users.noreply.github.com>
2024-12-21 13:36:17 +0100

    skillset

--- Portfolio-Demir/src/app/app.component.ts
+++ Portfolio-Demir/src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
-import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsetComponent } from './skillset/skillset.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, AboutComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    SkillsetComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio-Demir';
--- Portfolio-Demir/src/app/skillset/skillset.component.ts
+++ Portfolio-Demir/src/app/skillset/skillset.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skillset',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skillset.component.html',
-  styleUrl: './skillset.component.scss'
+  styleUrl: './skillset.component.scss',
 })
 export class SkillsetComponent {
-
+  skills = [
+    { svg: 'assets/img/skillsymbol/HTML.svg', label: 'HTML' },
+    { svg: 'assets/img/skillsymbol/CSS.svg', label: 'CSS' },
+    { svg: 'assets/img/skillsymbol/JavaScript.svg', label: 'JavaScript' },
+    {
+      svg: 'assets/img/skillsymbol/MaterialDesign.svg',
+      label: 'Material Design',
+    },
+    { svg: 'assets/img/skillsymbol/TypeScript.svg', label: 'TypeScript' },
+    { svg: 'assets/img/skillsymbol/Angular.svg', label: 'Angular' },
+    { svg: 'assets/img/skillsymbol/Firebase.svg', label: 'Firebase' },
+    { svg: 'assets/img/skillsymbol/GIT.svg', label: 'GIT' },
+    { svg: 'assets/img/skillsymbol/Rest-Api.svg', label: 'Rest-API' },
+    { svg: 'assets/img/skillsymbol/Scrum.svg', label: 'Scrum' },
+    {
+      svg: 'assets/img/skillsymbol/GrowthMindset.svg',
+      label: 'Growth Mindset',
+    },
+  ];
 }
