1e09a0ee5d2bb25086b098da10836e3c1d467274
Kadir-Demir-1988 <163757670+Kadir-Demir-1988@users.noreply.github.com>
2025-01-12 17:46:53 +0100

    skillsethover

--- Portfolio-Demir/src/app/skillset/skillset.component.ts
+++ Portfolio-Demir/src/app/skillset/skillset.component.ts
@@ -13,19 +13,28 @@ export class SkillsetComponent {
     { svg: 'assets/img/skillsymbol/HTML.svg', label: 'HTML' },
     { svg: 'assets/img/skillsymbol/CSS.svg', label: 'CSS' },
     { svg: 'assets/img/skillsymbol/JavaScript.svg', label: 'JavaScript' },
-    {
-      svg: 'assets/img/skillsymbol/MaterialDesign.svg',
-      label: 'Material Design',
-    },
+    { svg: 'assets/img/skillsymbol/MaterialDesign.svg', label: 'Material Design' },
     { svg: 'assets/img/skillsymbol/TypeScript.svg', label: 'TypeScript' },
     { svg: 'assets/img/skillsymbol/Angular.svg', label: 'Angular' },
     { svg: 'assets/img/skillsymbol/Firebase.svg', label: 'Firebase' },
     { svg: 'assets/img/skillsymbol/GIT.svg', label: 'GIT' },
     { svg: 'assets/img/skillsymbol/Rest-Api.svg', label: 'Rest-API' },
     { svg: 'assets/img/skillsymbol/Scrum.svg', label: 'Scrum' },
-    {
-      svg: 'assets/img/skillsymbol/GrowthMindset.svg',
-      label: 'Growth Mindset',
-    },
+    { svg: 'assets/img/skillsymbol/GrowthMindset.svg', label: 'Growth Mindset' }
   ];
+
+  isHovered = false; // Zustand für den Hover
+
+  onHover(isLast: boolean) {
+    if (isLast) {
+      this.isHovered = true;
+    }
+  }
+
+  onLeave(isLast: boolean) {
+    if (isLast) {
+      this.isHovered = false;
+    }
+  }
 }
+
